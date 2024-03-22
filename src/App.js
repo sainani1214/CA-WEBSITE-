20240650a9ec6d83262819865b2dd5a6eaab5506
sainani1214 <saikrishnamudhiraj79@gmail.com>
2024-03-22 12:40:56 +0530

    logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React from "react";
-import Header from "./Components/Header";
-import CarouselComponent from "./Components/CarouselComponent";
-import Content from "./Components/Description";
-import Services from "./Components/ServicesComponent";
-import KnowledgeCenter from "./Components/KnowledgeCenterComponent";
-import Footer from "./Components/Footer";
 import NavigationStack from "./Components/NavigationStack";
 
 const App = () => {
